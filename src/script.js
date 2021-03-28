24023f25eda4dc7e2e297d2029270a04b8539830
kkkatharine <74461046+kkkatharine@users.noreply.github.com>
2021-03-28 21:08:17 +0100

    added the search engine

--- src/script.js
+++ src/script.js
@@ -38,7 +38,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "21cae2f9a5ebbd41bf28f1b8f97e683b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "21cae2f9a5ebbd41bf28f1b8f97e683b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
